5f397f938a6cab3b6dab32031a3ef8dda8c63300
mLachaw <60004445+mLachaw@users.noreply.github.com>
2021-10-15 03:18:34 +0200

    Add files via upload
    Pjeojekt przerobiony tak aby był zgodny z wzorcem MVC

--- uiMap/src/test/java/mlachaw/uiMap/MainStageControllerTest.java
+++ uiMap/src/test/java/mlachaw/uiMap/MainStageControllerTest.java
@@ -1,30 +1,7 @@
 package mlachaw.uiMap;
 
-
-
-
-import static org.junit.Assert.assertArrayEquals;
-
-import java.util.ArrayList;
-import java.util.List;
-
 import org.junit.Test;
 
 public class MainStageControllerTest {
-
-	
-
-	@Test
-	public void separateOnlyIntTest() {
-		
-		
-			
-		}
-		
-	}
-
-	
-
-	
 
 }
